f8afedffb3811e83f18eb85ae243e1074f0397c0
ROOT-017 <nkwetachaterence17@gmail.com>
2023-09-22 14:15:38 +0100

    Stripe success and cancel urls made dynamic devepending on enviroment

--- controllers/checkoutController.js
+++ controllers/checkoutController.js
@@ -1,9 +1,18 @@
 const stripe = require("stripe")(process.env.STRIPE_SECRET_KEY);
+
+const successUrl =
+  process.env.Node_ENV === "development"
+    ? "http://localhost:3000/purchase/success"
+    : "https://e-commerce-sigma-lemon.vercel.app/purchase/success";
+const cancelUrl =
+  process.env.Node_ENV === "development"
+    ? "http://localhost:3000/?canceled=true"
+    : "https://e-commerce-sigma-lemon.vercel.app/?canceled=true";
 exports.checkoutSession = async (req, res, next) => {
   const { items, email } = req.body;
   if (!items)
     return res.send({
-      url: "https://e-commerce-sigma-lemon.vercel.app/?canceled=true",
+      url: cancelUrl,
     });
   const line_items = items.map((item) => {
     return {
@@ -25,11 +34,14 @@ exports.checkoutSession = async (req, res, next) => {
       payment_method_types: ["card"],
       customer_email: email,
       mode: "payment",
-      success_url: "https://e-commerce-sigma-lemon.vercel.app/purchase/success",
-      cancel_url: "https://e-commerce-sigma-lemon.vercel.app/?canceled=true",
+      success_url: successUrl,
+      cancel_url: cancelUrl,
     });
     res.send({ url: session.url });
   } catch (err) {
     console.log(err);
+    res.send({
+      url: cancelUrl,
+    });
   }
 };
